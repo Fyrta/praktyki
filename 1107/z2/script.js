bf77f5e3413404c8b3af78c7828f0e680bcc4344
pressure fried squid <115215977+Fyrta@users.noreply.github.com>
2023-11-27 22:24:25 +0100

    Update script.js

--- 1107/z2/script.js
+++ 1107/z2/script.js
@@ -1,10 +1,14 @@
-function palindrome(){
-    str = document.getElementById("text").value;
-    revstr = str.split("").reverse().join("");
-    
-    if(str == revstr){
-        alert("jest palindromem"); 
-    }else{
+function palindrome() {
+    var str = document.getElementById("text").value;
+    var revstr = "";
+
+    for (var i = str.length - 1; i >= 0; i--) {
+        revstr += str.charAt(i);
+    }
+
+    if (str === revstr) {
+        alert("jest palindromem");
+    } else {
         alert("nie jest palindromem");
     }
 }
