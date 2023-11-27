acd361d7c9d8014ca86f57dae1c227438207d11f
pressure fried squid <115215977+Fyrta@users.noreply.github.com>
2023-11-27 23:31:32 +0100

    Update script.js

--- 1108/z3/script.js
+++ 1108/z3/script.js
@@ -1,4 +1,27 @@
 var tab1 = ['Jan ', 'Paweł ', 'drugi'];
 var tab2 = ['był ', 'koneserem ', 'kremówek '];
+var tab3 = [];
+
+var words1 = tab1;
+var words2 = tab2;
+
+for (var i = 0; i < words1.length; i++) {
+    tab3.push(words1[i]);
+}
+
+for (var i = 0; i < words2.length; i++) {
+    tab3.push(words2[i]);
+}
+
+var result = [];
+
+for (var i = 0; i < tab3.length; i++) {
+    var word = tab3[i];
+    result.push(word);
+}
+
+document.write(result);
+var tab1 = ['Jan ', 'Paweł ', 'drugi'];
+var tab2 = ['był ', 'koneserem ', 'kremówek '];
 var tab3 = tab1.concat(tab2);
 document.write(tab3);
